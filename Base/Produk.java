22f82b9442fff02478390c946cfe81bf7b9ae7de
dindaaulyaa9@gmail.com <dindaauliaa9@gmail.com>
2026-04-20 20:23:46 +0700

    Enkapsulasi

--- Base/Produk.java
+++ Base/Produk.java
@@ -2,20 +2,39 @@ package Base;
 
 public class Produk {
     private String nama = "Produk A";
-    String kategori;
-    protected double harga;
-    public int stok;
+    private double harga;
+    private int stok;
 
-    public Produk(String nama, String kategori, double harga, int stok) {
+    public Produk(String nama, double harga, int stok) {
         this.nama = nama;
-        this.kategori = kategori;
         this.harga = harga;
         this.stok = stok;
     }
 
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+
+
     public void tampilkanInfo() {
         System.out.println("Nama Produk: " + nama);
-        System.out.println("Kategori: " + kategori);
         System.out.println("Harga: " + harga);
         System.out.println("Stok: " + stok);
         System.out.println("-------------------------");
--- Main.java
+++ Main.java
@@ -2,14 +2,20 @@ import Base.Produk;
 
 public class Main {
     public static void main(String[] args) {
-        Produk produk = new Produk("Mie Sedap", "Makanan", 3500, 100);
-        
-        System.out.println(produk.stok);
-        //System.out.println(produk.nama);
-        //System.out.println(produk.harga);
-        //System.out.println(produk.kategori);
+        Produk produk = new Produk("Indomie", 3000, 200);
 
-        produk.tampilkanInfo();
+        System.out.println(produk.getNama());
+        System.out.println(produk.getHarga());
+        System.out.println(produk.getStok());
+
+        produk.setHarga(3500);
+        produk.setStok(100);
+
+        System.out.println(produk.getNama());
+        System.out.println(produk.getHarga());
+        System.out.println(produk.getStok());
+        
+
     }
     
 }
